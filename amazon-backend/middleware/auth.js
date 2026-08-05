f8c0393fc27df405034103615c993fa622209eaf
srinivas chow <illurisrinivas2006@gmail.com>
2026-08-05 11:12:09 +0530

    Add auth debug logs

--- amazon-backend/middleware/auth.js
+++ amazon-backend/middleware/auth.js
@@ -1,20 +1,46 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-export function protect(req,res,next){
+// export function protect(req,res,next){
+//     const token = req.cookies.jwt;
+//     if(!token){
+//        return  res.status(401).json({
+//             message : "Not Authorised"
+//         })
+//     }
+//     try{
+//         const decoded = jwt.verify(token,process.env.JWT_SECRET);
+//         req.user = decoded;
+//         next();
+//     }
+//     catch (err) {
+//     return res.status(401).json({
+//         message: "Invalid token"
+//     });
+// }
+// }
+
+export function protect(req, res, next) {
+
+    console.log("Cookies:", req.cookies);
+
     const token = req.cookies.jwt;
-    if(!token){
-       return  res.status(401).json({
-            message : "Not Authorised"
-        })
+
+    if (!token) {
+        console.log("NO TOKEN");
+        return res.status(401).json({
+            message: "Not Authorised"
+        });
     }
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("TOKEN VERIFIED");
         req.user = decoded;
         next();
+    } catch (err) {
+        console.log("VERIFY ERROR:", err.message);
+        return res.status(401).json({
+            message: "Invalid token"
+        });
     }
-    catch (err) {
-    return res.status(401).json({
-        message: "Invalid token"
-    });
-}
 }
